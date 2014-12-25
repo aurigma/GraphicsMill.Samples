40295f11edff4eb5b7f4f6a62620672ee748a8c7
Fedor Skvortsov <fskvortsov@gmail.com>
2014-12-25 16:27:32 +0600

    Added formatted text

--- D.FontAndText/D.07.FormattedText/FormattedTextExample.cs
+++ D.FontAndText/D.07.FormattedText/FormattedTextExample.cs
@@ -12,6 +12,25 @@ class FormattedTextExample
 
 	private static void DrawFormattedText()
 	{
-		//TODO: This feature will be available in the version 7.2
+		using (var bitmap = new Bitmap(600, 400, PixelFormat.Format24bppRgb, new RgbColor(255, 255, 255, 255)))
+		using (var graphics = bitmap.GetAdvancedGraphics())
+		{
+			var brush = new SolidBrush(RgbColor.Black);
+
+			var dummyText = @"Lorem ipsum dolor sit <span style='color:red;'>amet</span>, consectetur adipiscing elit. " + 
+				@"<span style='bold:true;underline:true;color:rgb(0,0,255)'>Cras</span> elementum quam ac nisi varius gravida. Mauris ornare, " +
+				@"<span style='font-name:Times New Roman;font-size:60pt;pen-color:green;pen-width:1px;color:yellow;'>dolor</span> et scelerisque " +
+				@"volutpat, <span style='italic:true;color:gray;'>enim urna commodo odio, <span style='color:green;'>consequat</span> fermentum sem arcu</span> sit amet nisl. " + 
+				@"Aliquam tincidunt id neque in gravida. Mauris mollis est vulputate suscipit facilisis.";
+
+			var boundedText = new BoundedText(dummyText, graphics.CreateFont("Verdana", 26f), brush)
+			{
+				Rectangle = new System.Drawing.RectangleF(20f, 20f, 560f, 360f)
+			};
+
+			graphics.DrawText(boundedText);
+
+			bitmap.Save("../../../../_Output/DrawFormattedText.png");
+		}
 	}
 }
