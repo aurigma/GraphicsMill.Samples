05c8f89b10f143b4b0e6befd07d27a836051545a
radik <radik@aurigma.com>
2015-08-24 12:26:34 +0600

    0020890: В сампл B.01 Resize добавить пример использования UnsharpMask для повышения четкости
    fixed

--- B.FiltersAndTransforms/B.01.Resize/ResizeExample.cs
+++ B.FiltersAndTransforms/B.01.Resize/ResizeExample.cs
@@ -12,6 +12,9 @@ class ResizeExample
 		ResizeMemoryFriendly();
 		ResizeToBitmapMemoryFriendly();
 
+        ResizeSharp();
+        ResizeSharpMemoryFriendly();
+
 		//See also F.Metadata --> F.02. Thumbnail from EXIF
 	}
 
@@ -57,5 +60,34 @@ class ResizeExample
 			bitmap.Save("../../../../_Output/ResizeToBitmapMemoryFriendly.jpg");
 		}
 	}
+
+
+    /// <summary>
+    /// Resizes image and maintains original sharpness (like Bicubic Sharper mode in Photoshop)
+    /// </summary>
+    private static void ResizeSharp()
+    {
+        using (var bitmap = new Bitmap("../../../../_Input/Chicago.jpg"))
+        {
+            bitmap.Transforms.Resize(320, 0, ResizeInterpolationMode.High);
+            bitmap.Transforms.UnsharpMask(0.35f, 1, 0);
+            bitmap.Save("../../../../_Output/ResizeSharp.jpg");
+        }
+    }
+
+
+    /// <summary>
+    /// Resizes image and maintains original sharpness using memory-friendly Pipeline API (like Bicubic Sharper mode in Photoshop)
+    /// </summary>
+    private static void ResizeSharpMemoryFriendly()
+    {
+        using (var reader = ImageReader.Create("../../../../_Input/Chicago.jpg"))
+        using (var resize = new Resize(320, 0, ResizeInterpolationMode.High))
+        using (var unsharpMask = new UnsharpMask(0.35f, 1, 0))
+        using (var writer = ImageWriter.Create("../../../../_Output/ResizeSharpMemoryFriendly.jpg"))
+        {
+            Pipeline.Run(reader + resize + unsharpMask + writer);
+        }
+    }
 }
 
